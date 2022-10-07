a00d9c1cb93fefd0147946e302e624d7d6891c62
dan brackenbury <daniel.brackenbury@gmail.com>
2022-10-06 23:42:14 -0400

    added aws support, removed JawsDB support

--- config/connection.js
+++ config/connection.js
@@ -2,25 +2,34 @@
 const Sequelize = require('sequelize');
 const dotenv = require('dotenv').config();
 
-// create a connection to the database
-// init sequelize var
-let sequelize;
+// environment variables
+let host;
+let db;
+let user;
+let pass;
+let port;
 
-// check if a JawsDB URL is active in the .env
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.RDS_HOSTNAME) {
+    host = process.env.RDS_HOSTNAME;
+    db = process.env.RDS_DB_NAME;
+    user = process.env.RDS_USERNAME;
+    pass = process.env.RDS_PASSWORD;
+    port = process.env.RDS_PORT;
 } else {
     // store variables from dotenv
-    const db = process.env.DB_NAME;
-    const user = process.env.SQL_USER;
-    const pass = process.env.SQL_PASS;
-
-    sequelize = new Sequelize(db, user, pass, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
+    host = 'localhost';
+    db = process.env.DB_NAME;
+    user = process.env.SQL_USER;
+    pass = process.env.SQL_PASS;
+    port = 3306;
 }
+
+// create a connection to the database
+const sequelize = new Sequelize(db, user, pass, {
+    host: host,
+    dialect: 'mysql',
+    port: port
+});
 
 // export sequelize connection for use in other scripts
 module.exports = sequelize;
