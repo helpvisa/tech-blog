75c2565353b3d6b2aec9b08e8cb798c791a66269
dan brackenbury <daniel.brackenbury@gmail.com>
2022-10-07 02:04:04 -0400

    rolled back changes

--- config/connection.js
+++ config/connection.js
@@ -3,36 +3,24 @@ const Sequelize = require('sequelize');
 const dotenv = require('dotenv').config();
 
 // environment variables
-let host;
-let db;
-let user;
-let pass;
-let port;
+let sequelize;
 
-if (process.env.RDS_HOSTNAME) {
-    host = process.env.RDS_HOSTNAME;
-    db = process.env.RDS_DB_NAME;
-    user = process.env.RDS_USERNAME;
-    pass = process.env.RDS_PASSWORD;
-    port = process.env.RDS_PORT;
+if (process.env.JAWS_DB_URL) {
+    sequelize = new Sequelize(process.env.JAWS_DB_URL);
 } else {
-    // store variables from dotenv
-    host = 'localhost';
-    db = process.env.DB_NAME;
-    user = process.env.SQL_USER;
-    pass = process.env.SQL_PASS;
-    port = 3306;
+    // environment variables
+    const db = process.env.DB_NAME;
+    const user = process.env.SQL_USER;
+    const pass = process.env.SQL_PASS;
+    // create a connection to the database
+    sequelize = new Sequelize(db, user, pass, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
 }
 
-// create a connection to the database
-const sequelize = new Sequelize(db, user, pass, {
-    host: host,
-    dialect: 'mysql',
-    dialectOptions: {
-        ssl: 'Amazon RDS'
-    },
-    port: port
-});
+
 
 // export sequelize connection for use in other scripts
 module.exports = sequelize;
